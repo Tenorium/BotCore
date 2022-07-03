46dc3da11efc2f560146470b44d8fcfe5596c7ca
Artem S <admin@tenorium.net>
2022-07-03 12:27:03 +0300

    System modules replaced with submodules

--- system-modules/cli/errors/CliCommandAlreadyExist.js
+++
@@ -1,7 +0,0 @@
-export default class CliCommandAlreadyExist extends Error {
-    constructor() {
-        super('Core already initialized!');
-        this.name = 'CommandAlreadyExistError';
-        this.stack = (new Error).stack;
-    }
-}
