671f70220295e61b2d74cc47e349aaa5203e0473
Artem S <admin@tenorium.net>
2022-07-04 17:15:32 +0300

    Update

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import Core from "./core/core.js";
-import config from "./config/core.config.js";
 import colors from "colors";
 import i18n from "i18n";
 import {dirname} from "path";
 import {createDefaultData} from "./system-modules/pkg/pkg-util.js";
+import path from "path";
+import os from "os";
+import AutoGitUpdate from "auto-git-update";
 
 global.basePath = dirname(new URL('', import.meta.url).pathname);
+
+/**
+ *
+ * @type {Config}
+ */
+let config = {
+    repository: "https://github.com/Tenorium/BotCore",
+    branch: "master",
+    tempLocation: path.join(os.tmpdir(), 'botcore')
+};
+
+let updater = new AutoGitUpdate(config);
+
+await updater.autoUpdate();
 
 i18n.configure({
     locales: ['en', 'ru'],
