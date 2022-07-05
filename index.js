a658358a2e2bed1dbd1794d13235656ad48a2de3
Artem S <admin@tenorium.net>
2022-07-05 19:47:24 +0300

    Config example replaced with interactive wizard

--- index.js
+++ index.js
@@ -7,10 +7,52 @@ import path from "path";
 import os from "os";
 import AutoGitUpdate from "auto-git-update";
 import * as fs from "fs";
+import readline from "readline";
 
 global.basePath = dirname(new URL('', import.meta.url).pathname);
 
-let config = JSON.parse(fs.readFileSync(path.join(basePath, 'config/config.json')));
+let configPath = path.join(basePath, 'config/config.json');
+
+if (!fs.existsSync(configPath)) {
+    console.log("Config not exist.");
+    console.log("Starting configuration wizard...");
+
+    let defaultConfig = {
+        logger: {
+            debug: false,
+            dateformat: "DD.MM.YYYY HH:mm:ss"
+        },
+        "client": {
+            "intents": [
+                32767
+            ]
+        }
+    }
+
+    let rl = readline.createInterface(process.stdin, process.stdout);
+
+    let locale = await rl.question("Select language(default \"en\", available \"en\", \"ru\"): ");
+    if (!['en', 'ru'].includes(locale)) {
+        locale = 'en';
+    }
+
+    let token = await rl.question("Enter bot token: ");
+    let prefix = await rl.question("Enter prefix(default \"//\"): ");
+
+    defaultConfig = {
+        ...defaultConfig,
+        locale,
+        token,
+        prefix
+    }
+
+    rl.close();
+    fs.writeFileSync(configPath, JSON.stringify(defaultConfig));
+
+    console.log("Config saved to config/config.json.")
+}
+
+let config = JSON.parse(fs.readFileSync(configPath));
 
 /**
  *
