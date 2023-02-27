46fe6ebd462d4fb6b6b90dfe6b4c51a1e33b5a3a
Artem S <admin@tenorium.net>
2023-02-27 12:07:45 +0200

    Removed package manager

--- old/pkg/error/PackageVersionAlreadyInstalled.js
+++
@@ -1,6 +0,0 @@
-export class PackageVersionAlreadyInstalled extends Error {
-    constructor(message) {
-        super(message);
-        this.name = "PackageVersionAlreadyInstalled";
-    }
-}
--- old/pkg/error/VersionOfPackageNotFound.js
+++
@@ -1,6 +0,0 @@
-export class VersionOfPackageNotFound extends Error {
-    constructor(message) {
-        super(message);
-        this.name = 'VersionOfPackageNotFound';
-    }
-}
