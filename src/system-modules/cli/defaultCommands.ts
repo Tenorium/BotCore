e24e89dfc5a11662f68f84eefce8fcbb70619dc0
HACKERPRO17 <sartyom532@gmail.com>
2025-04-09 17:30:29 +0300

    refactor(commandManager): Removed command for "command manager"

--- src/system-modules/cli/defaultCommands.ts
+++ src/system-modules/cli/defaultCommands.ts
@@ -1,10 +1,8 @@
-import CliModule from './cli.js'
+import type CliModule from './cli.js'
 import { addShutdownCommand } from './defaultCommands/shutdown.js'
-import { addCommandManagerCommand } from './defaultCommands/command.js'
 import { addModuleManagerCommand } from './defaultCommands/module.js'
 
 export const run = function (cli: CliModule): void {
   addShutdownCommand(cli)
-  addCommandManagerCommand(cli)
   addModuleManagerCommand(cli)
 }
